9dfac61678c8ba65d1c6a88b708d5752cfb7a617
shamssumon3836 <49681181+shamssumon3836@users.noreply.github.com>
2020-05-15 16:50:52 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ $('.sd').click(function(){
 $('.close-modal').click(function(){
     elements.removeClass('active');
 });
+
+
+
+
+
+    $(document).ready(function(){
+        $("#testimonial-slider").owlCarousel({
+            items:3,
+            itemsDesktop:[1000,3],
+            itemsDesktopSmall:[980,2],
+            itemsTablet:[768,2],
+            itemsMobile:[650,1],
+            pagination:true,
+            navigation:false,
+            slideSpeed:1000,
+            autoPlay:true
+        });
+    });
--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,13 @@
 // Powered by WOW  jQuery library
 new WOW().init();
 
+$("#slideshow > div:gt(0)").hide();
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
